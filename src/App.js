1deaa159f031997f6764b516c9c859022160387d
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-10 17:21:34 -0300

    Lifecicle methods

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
   state = {
-    name: "Marcio Andrade",
     counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "O corpo 1",
+      },
+      {
+        id: 2,
+        title: "O título 2",
+        body: "O corpo 2",
+      },
+      {
+        id: 3,
+        title: "O título 3",
+        body: "O corpo 3",
+      },
+    ],
   };
+  timeOutUpdate = null;
 
-  handlePClick = () => {
-    this.setState({ name: "Júnior" });
-  };
+  componentDidMount() {
+    this.handleTimeout();
+  }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.handleTimeout);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "O título mudou";
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={this.handleAClick}
-          >
-            Este é o link
-          </a>
-        </header>
+        <h1>{counter}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
-
-// function App() {
-//
-// }
 
 export default App;
